5286f5a7d71ad97f9e28a4a398281d370702b54e
danielclaudio08 <u262229862@byui.edu>
2026-07-16 04:08:47 +0800

    Week 2 Learning Activity

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import { testConnection } from './src/models/db.js';
+import { getAllOrganizations } from './src/models/organizations.js';
 
 // Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -30,34 +32,50 @@ app.set('views', path.join(__dirname, 'src/views'));
  * Routes
  */
 app.get('/', async (req, res) => {
+
+  const title = 'Home Page';
+
   res.render('home', {
-    title: 'Home Page',
+    title,
     currentPage: 'home'
   });
 });
 
 app.get('/organizations', async (req, res) => {
+  const organizations = await getAllOrganizations();
+  const title = 'Our Partner Organizations';
+
   res.render('organizations', {
-    title: 'Our Partner Organizations',
-    currentPage: 'organizations'
+    title,
+    currentPage: 'organizations',
+    organizations
   });
 });
 
 app.get('/projects', async (req, res) => {
+  const title = 'Service Projects';
+
   res.render('projects', {
-    title: 'Service Projects',
+    title,
     currentPage: 'projects'
   });
 });
 
 app.get('/categories', async (req, res) => {
+  const title = 'Service Projects Categories';
+
   res.render('categories', {
-    title: 'Service Projects Categories',
+    title,
     currentPage: 'categories'
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://127.0.0.1:${PORT}`);
-  console.log(`Environment: ${NODE_ENV}`);
+app.listen(PORT, async () => {
+  try {
+    await testConnection();
+    console.log(`Server is running at http://127.0.0.1:${PORT}`);
+    console.log(`Environment: ${NODE_ENV}`);
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  }
 });
